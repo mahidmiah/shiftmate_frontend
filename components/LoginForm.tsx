14775ccb9512e55558fa9b87c15569bdf5ddc3a4
Mahid Miah <miah.mahid@outlook.com>
2023-12-05 23:39:38 +0000

    testing: removed origin on api requests

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -54,8 +54,7 @@ function LoginForm() {
             method: 'POST',
             body: JSON.stringify({email, password}),
             headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://localhost:4000' 
+                'Content-Type': 'application/json'
             },
             credentials: "include"
         });
