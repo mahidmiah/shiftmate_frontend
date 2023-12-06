d39aa4c11745582b282a82c6bda91ade11af28da
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 17:06:10 +0000

    trial and error

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { ro } from "date-fns/locale";
 import { useRouter } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest, response: NextResponse) {
+export function middleware(request: NextRequest, response: NextResponse, next: () => void) {
     const router = useRouter()
     const { pathname } = request.nextUrl;
     const cookies = request.cookies as unknown as { [key: string]: string };
@@ -13,11 +13,13 @@ export function middleware(request: NextRequest, response: NextResponse) {
         // If the user is logged in and trying to access the auth routes, redirect to /app/home
         router.push('/app/home')
         // console.log('redirecting to /app/home')
+        next()
     }
 
     if (!isLoggedIn && pathname.startsWith('/app/')) {
         // If the user is not logged in and trying to access the app routes, redirect to /auth/login
         router.push('/auth/login')
         // console.log('redirecting to /auth/login')
+        next()
     }
 }
