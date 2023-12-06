bea468c9fae2cff073e37b7c16f3a3e991515496
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 16:46:47 +0000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const token = request.cookies.get('access_token')?.value || '';
+    const cookies = request.cookies as unknown as { [key: string]: string };
+    const token = cookies['access_token'] || '';
     const isLoggedIn = token !== '';
 
     if (isLoggedIn && pathname.startsWith('/auth/')) {
-        // If the user is not logged in and trying to access the default / route, redirect to /auth/login
+        // If the user is logged in and trying to access the auth routes, redirect to /app/home
         return NextResponse.redirect(new URL('/app/home', request.nextUrl));
     }
 
     if (!isLoggedIn && pathname.startsWith('/app/')) {
-        // If the user is not logged in and trying to access the default / route, redirect to /auth/login
+        // If the user is not logged in and trying to access the app routes, redirect to /auth/login
         return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
     }
 }
