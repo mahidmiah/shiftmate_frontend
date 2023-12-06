2977cd238cf2f6299e9f51199b43d138ed280429
Mahid Miah <miah.mahid@outlook.com>
2023-12-06 17:47:01 +0000

    disabled middleware again

--- middleware.ts
+++ middleware.ts
@@ -17,15 +17,15 @@ export function middleware(request: NextRequest, response: NextResponse) {
     console.log('middleware.ts: request.nextUrl.pathname: ', request.nextUrl.pathname)
     console.log('---------------------------------------------------------------------------')
 
-    if (isLoggedIn && pathname.startsWith('/auth/')) {
-        // If the user is logged in and trying to access the auth routes, redirect to /app/home
-        return NextResponse.redirect(new URL('/app/home', request.nextUrl));
-        // console.log('redirecting to /app/home')
-    }
+    // if (isLoggedIn && pathname.startsWith('/auth/')) {
+    //     // If the user is logged in and trying to access the auth routes, redirect to /app/home
+    //     return NextResponse.redirect(new URL('/app/home', request.nextUrl));
+    //     // console.log('redirecting to /app/home')
+    // }
 
-    if (!isLoggedIn && pathname.startsWith('/app/')) {
-        // If the user is not logged in and trying to access the app routes, redirect to /auth/login
-        return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
-        // console.log('redirecting to /auth/login')
-    }
+    // if (!isLoggedIn && pathname.startsWith('/app/')) {
+    //     // If the user is not logged in and trying to access the app routes, redirect to /auth/login
+    //     return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
+    //     // console.log('redirecting to /auth/login')
+    // }
 }
